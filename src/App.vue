ce0efa4afc00bbc73fb734bfac524661efb06353
James <101947821+pmjamez@users.noreply.github.com>
2022-09-22 23:23:52 -0700

    linked openweather api key and quicksand fonts

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
-<script setup>
-</script>
-
 <template>
-  <header>
-    
-    
-   
+  <div id = "app">
+  </div>
 
-
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
+<script>
+import axios from "axios"
+
+export default {
+  name: "App", 
+  data() {
+    return{
+      APIkey: "a9d8874be240443cdb782496cfa287d5",
+      city: "Detroit",
+    };
+
+  },
+  created() {
+    this.getCurrentWeather();
+
+  },
+
+  methods: {
+    getCurrentWeather() {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=imperial&APPID=${this.APIkey}`
+      ).then(res => {
+        console.log(res.data);
+      });
+      
+    },
+  },
+};
+</script>
+
+
+
+<style lang = "scss">
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Quicksand", sans-serif;
+
+  }
 
 
 </style>
